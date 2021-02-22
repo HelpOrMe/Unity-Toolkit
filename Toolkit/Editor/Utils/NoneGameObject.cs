36a6a92366a3426df0a8330c962dbf50fa6dc347
HelpOrMe <helporme.dev@gmail.com>
2021-02-22 23:50:54 +0200

    Add NoneGameObject util

---
+++ Toolkit/Editor/Utils/NoneGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Toolkit.Editor.Utils
+{
+    public class NoneGameObject
+    {
+        public const string PrefabPath = "Packages/Toolkit/Resources/NoneGameObject.prefab";
+
+        public static GameObject Instance => _instance ??= 
+            PrefabUtility.LoadPrefabContents(Path.GetFullPath(PrefabPath));
+        private static GameObject _instance;
+    }
+}
