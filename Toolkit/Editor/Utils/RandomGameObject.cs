f25d39b5bc2869665646fbad53072c9079b7a730
HelpOrMe <helporme.dev@gmail.com>
2021-02-24 02:33:58 +0200

    Replace NoneGameObject with RandomGameObject

---
+++ Toolkit/Editor/Utils/RandomGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ToolkitEditor.Utils
+{
+    public class RandomGameObject
+    {
+        public const string PrefabPath = "Packages/Toolkit/Resources/NoneGameObject.prefab";
+
+        public static GameObject Instance
+        {
+            get
+            {
+                if (SceneGameObjects.Count == 0)
+                    SceneManager.GetActiveScene().GetRootGameObjects(SceneGameObjects);
+                return SceneGameObjects.Count > 0 ? SceneGameObjects[0] : new GameObject();
+            }
+        }
+
+        private static readonly List<GameObject> SceneGameObjects = new List<GameObject>();
+    }
+}
